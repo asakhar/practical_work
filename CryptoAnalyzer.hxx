630ad42207d1239822717aa66b3f592ca8a27ecd
asakhar <a@sakhar.ru>
2021-01-26 16:36:37 +0300

    analysis for affine

--- CryptoAnalyzer.hxx
+++ CryptoAnalyzer.hxx
@@ -11,25 +11,28 @@
 #include <stdexcept>
 #include <unordered_map>
 #include <vector>
-template <typename Char_t>
-struct MyHash
-{
-  std::size_t operator()(std::pair<Char_t, Char_t> const& s) const noexcept
-  {
-    auto h1 = std::hash<Char_t>{}(s.first);
-    auto h2 = std::hash<Char_t>{}(s.second);
-    return h1 ^ (h2 << 1); // or use boost::hash_combine (see Discussion)
-  }
-};
-template <typename Char_t>
-struct MyPred
-{
-  bool operator()(std::pair<Char_t, Char_t> const& s1,
-                  std::pair<Char_t, Char_t> const& s2) const noexcept
-  {
-    return s1.first == s2.first && s1.second == s2.second;
-  }
-};
+
+#include <bits/stdc++.h>
+
+// template <typename Char_t>
+// struct MyHash
+// {
+//   std::size_t operator()(std::pair<Char_t, Char_t> const& s) const noexcept
+//   {
+//     auto h1 = std::hash<Char_t>{}(s.first);
+//     auto h2 = std::hash<Char_t>{}(s.second);
+//     return h1 ^ (h2 << 1); // or use boost::hash_combine (see Discussion)
+//   }
+// };
+// template <typename Char_t>
+// struct MyPred
+// {
+//   bool operator()(std::pair<Char_t, Char_t> const& s1,
+//                   std::pair<Char_t, Char_t> const& s2) const noexcept
+//   {
+//     return s1.first == s2.first && s1.second == s2.second;
+//   }
+// };
 template <typename _Key, typename _Tp, typename _Hash = std::hash<_Key>,
           typename _Pred = std::equal_to<_Key>>
 class unordered_map_with_default_value
@@ -60,6 +63,7 @@ public:
       return m_default;
     return _Base::at(key);
   }
+  _Tp get_default() const { return m_default; }
 
 private:
   _Tp m_default;
@@ -69,13 +73,13 @@ class Analyzer
 {
 private:
   using Char_t = char;
-  unordered_map_with_default_value<Char_t, double> m_symb2freq{0};
+  // unordered_map_with_default_value<Char_t, double> m_symb2freq{0};
   std::vector<
       unordered_map_with_default_value<std::basic_string<Char_t>, double>>
       m_ngm2freq;
-  unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
-                                   MyHash<Char_t>, MyPred<Char_t>>
-      m_bigr2freq{0};
+  // unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
+  //                                  MyHash<Char_t>, MyPred<Char_t>>
+  //     m_bigr2freq{0};
 
 public:
   static void dump_double(std::ostream& st, double dbl)
@@ -89,78 +93,22 @@ public:
   }
 
   Analyzer() = default;
-  Analyzer(std::string_view freqs_filename, std::string_view bigrs_filename,
-           std::string_view ngramms_filename, size_t max_n = 3)
+  Analyzer(std::string_view ngramms_filename, size_t max_n = 3)
   {
-    load_freqs(freqs_filename);
-    load_bigrs(bigrs_filename);
+    // load_freqs(freqs_filename);
+    // load_bigrs(bigrs_filename);
     for (auto i = 1ul; i <= max_n; i++)
     {
       m_ngm2freq.emplace_back(0);
       load_ngrs(ngramms_filename, i);
     }
   };
-  void load_freqs(std::string_view filename)
-  {
-    std::ifstream file{filename.data()};
-    if (file.fail())
-      throw std::runtime_error("File not found.");
-    m_symb2freq.clear();
-    while (!file.bad() && !file.eof())
-    {
-      Char_t symb;
-      std::basic_string<Char_t> symb1;
-      double freq;
-      std::getline(file, symb1, ';');
-      // file >> symb;
-      if (file.bad() || file.eof())
-        break;
-      assert(symb1.size() == 1 || symb1.size() == 2 && "Input error");
-      if (symb1.size() == 1)
-        symb = symb1[0];
-      else
-      {
-        if (symb1[1] == 'n')
-          symb = '\n';
-        else if (symb1[1] == 'c')
-          symb = ';';
-      }
-      // file.get();
-      restore_double(file, freq);
-      // file >> freq;
-      file.get();
-      m_symb2freq[symb] = freq;
-    }
-    file.close();
-  }
-  void load_bigrs(std::string_view filename)
-  {
-    std::ifstream file{filename.data()};
-    if (file.fail())
-      throw std::runtime_error("File not found.");
-    m_bigr2freq.clear();
-    while (!file.bad() && !file.eof())
-    {
-      std::basic_string<Char_t> symb1;
-      double freq;
-      std::getline(file, symb1, ';');
-      if (file.bad() || file.eof())
-        break;
-      assert(symb1.size() == 2 && "Input error");
-      // file.get();
-      restore_double(file, freq);
-      file.get();
-      m_bigr2freq[{symb1[0], symb1[1]}] = freq;
-    }
-    file.close();
-  }
 
   void load_ngrs(std::string_view filename, size_t length)
   {
     std::ifstream file{filename.data()};
     if (file.fail())
       throw std::runtime_error("File not found.");
-    // m_bigr2freq.clear();
     while (!file.bad() && !file.eof())
     {
       std::basic_string<Char_t> symb1;
@@ -169,7 +117,6 @@ public:
       if (file.bad() || file.eof())
         break;
 
-      // file.get();
       restore_double(file, freq);
       file.get();
       if (symb1.size() == length)
@@ -178,52 +125,123 @@ public:
     file.close();
   }
 
-  auto count_frequencies(std::basic_string_view<Char_t> msg) const
-  {
-    unordered_map_with_default_value<Char_t, double> resolved_freqs{0};
-    for (auto ch : msg)
-      resolved_freqs[ch]++;
-    for (auto& item : resolved_freqs)
-      item.second /= static_cast<double>(msg.size());
-    return std::move(resolved_freqs);
-  }
-
-  void save_freqs(std::string_view filename,
-                  unordered_map_with_default_value<Char_t, double> freqs) const
-  {
-    std::ofstream file{filename.data()};
-    for (auto& item : freqs)
+  /*
+    void load_freqs(std::string_view filename)
     {
-      if (item.first == (Char_t)'\n')
-        file << "\\n;";
-      else if (item.first == (Char_t)';')
-        file << "\\c;";
-      else
-        file << item.first << ";";
-      dump_double(file, item.second);
-      file << "\n";
+      std::ifstream file{filename.data()};
+      if (file.fail())
+        throw std::runtime_error("File not found.");
+      m_symb2freq.clear();
+      while (!file.bad() && !file.eof())
+      {
+        Char_t symb;
+        std::basic_string<Char_t> symb1;
+        double freq;
+        std::getline(file, symb1, ';');
+        // file >> symb;
+        if (file.bad() || file.eof())
+          break;
+        assert(symb1.size() == 1 || symb1.size() == 2 && "Input error");
+        if (symb1.size() == 1)
+          symb = symb1[0];
+        else
+        {
+          if (symb1[1] == 'n')
+            symb = '\n';
+          else if (symb1[1] == 'c')
+            symb = ';';
+        }
+        // file.get();
+        restore_double(file, freq);
+        // file >> freq;
+        file.get();
+        m_symb2freq[symb] = freq;
+      }
+      file.close();
     }
-    file.close();
-  }
-
-  auto count_bigramms(std::basic_string_view<Char_t> msg) const
-  {
-    unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
-                                     MyHash<Char_t>, MyPred<Char_t>>
-        resolved_freqs{0};
-    for (Char_t prev = '\0'; auto ch : msg)
+    void load_bigrs(std::string_view filename)
     {
-      if (prev != '\0' && prev != '\n' && prev != ';' && prev != ' ' &&
-          prev != ',' && ch != '.' && ch != '\n' && ch != ';' && ch != ' ' &&
-          ch != ',')
-        resolved_freqs[{prev, ch}]++;
-      prev = ch;
+      std::ifstream file{filename.data()};
+      if (file.fail())
+        throw std::runtime_error("File not found.");
+      m_bigr2freq.clear();
+      while (!file.bad() && !file.eof())
+      {
+        std::basic_string<Char_t> symb1;
+        double freq;
+        std::getline(file, symb1, ';');
+        if (file.bad() || file.eof())
+          break;
+        assert(symb1.size() == 2 && "Input error");
+        // file.get();
+        restore_double(file, freq);
+        file.get();
+        m_bigr2freq[{symb1[0], symb1[1]}] = freq;
+      }
+      file.close();
     }
-    for (auto& item : resolved_freqs)
-      item.second /= static_cast<double>(msg.size() + 1);
-    return std::move(resolved_freqs);
-  }
+    auto count_frequencies(std::basic_string_view<Char_t> msg) const
+    {
+      unordered_map_with_default_value<Char_t, double> resolved_freqs{0};
+      for (auto ch : msg)
+        resolved_freqs[ch]++;
+      for (auto& item : resolved_freqs)
+        item.second /= static_cast<double>(msg.size());
+      return std::move(resolved_freqs);
+    }
 
+    void save_freqs(std::string_view filename,
+                    unordered_map_with_default_value<Char_t, double> freqs)
+    const
+    {
+      std::ofstream file{filename.data()};
+      for (auto& item : freqs)
+      {
+        if (item.first == (Char_t)'\n')
+          file << "\\n;";
+        else if (item.first == (Char_t)';')
+          file << "\\c;";
+        else
+          file << item.first << ";";
+        dump_double(file, item.second);
+        file << "\n";
+      }
+      file.close();
+    }
+
+    auto count_bigramms(std::basic_string_view<Char_t> msg) const
+    {
+      unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
+                                       MyHash<Char_t>, MyPred<Char_t>>
+          resolved_freqs{0};
+      for (Char_t prev = '\0'; auto ch : msg)
+      {
+        if (prev != '\0' && prev != '\n' && prev != ';' && prev != ' ' &&
+            prev != ',' && ch != '.' && ch != '\n' && ch != ';' && ch != ' ' &&
+            ch != ',')
+          resolved_freqs[{prev, ch}]++;
+        prev = ch;
+      }
+      for (auto& item : resolved_freqs)
+        item.second /= static_cast<double>(msg.size() + 1);
+      return std::move(resolved_freqs);
+    }
+    void save_bigramms(
+        std::string_view filename,
+        unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
+                                         MyHash<Char_t>, MyPred<Char_t>>
+            freqs) const
+    {
+      std::ofstream file{filename.data()};
+      for (auto& item : freqs)
+      {
+        file << item.first.first << item.first.second << ";";
+        dump_double(file, item.second);
+        file << "\n";
+      }
+      file.close();
+    }
+  */
   auto count_ngramms(std::basic_string_view<Char_t> msg, size_t length) const
   {
     unordered_map_with_default_value<std::basic_string<Char_t>, double>
@@ -235,7 +253,6 @@ public:
       if (val == length)
       {
         char* copy = new char[length + 1];
-        // prev.getline(copy, length + 1);
         prev.read(copy, length);
         copy[length] = 0;
         prev.clear();
@@ -245,7 +262,10 @@ public:
       }
       if (length == 1 && ch != '\0' && ch != ';')
       {
-        prev.put(ch);
+        if constexpr (std::is_same_v<Char_t, char>)
+          prev.put(static_cast<Char_t>(std::tolower(ch)));
+        else
+          prev.put(ch);
       }
       else if (ch != '\0' && ch != ' ' && ch != '\n' && ch != '.' &&
                ch != ',' && ch != ':' && ch != ';' && ch != '\"' && ch != '\'')
@@ -254,7 +274,9 @@ public:
           prev.put(static_cast<Char_t>(std::tolower(ch)));
         else
           prev.put(ch);
-      }else {
+      }
+      else
+      {
         prev.str("");
       }
     }
@@ -278,26 +300,8 @@ public:
     file.close();
   }
 
-  void save_bigramms(
-      std::string_view filename,
-      unordered_map_with_default_value<std::pair<Char_t, Char_t>, double,
-                                       MyHash<Char_t>, MyPred<Char_t>>
-          freqs) const
-  {
-    std::ofstream file{filename.data()};
-    for (auto& item : freqs)
-    {
-      file << item.first.first << item.first.second << ";";
-      dump_double(file, item.second);
-      file << "\n";
-    }
-
-    file.close();
-  }
-  template <typename _Key, typename... _Args>
-  double calc_mean_squared_error(
-      unordered_map_with_default_value<_Key, double, _Args...> const& lhs,
-      unordered_map_with_default_value<_Key, double, _Args...>& rhs) const
+  template <typename _Ty>
+  static double calc_mean_squared_error(_Ty const& lhs, _Ty const& rhs)
   {
     long double res = 0;
     for (auto& kv : rhs)
@@ -306,22 +310,44 @@ public:
     return static_cast<double>(res / rhs.size());
   }
 
-  template <typename _Key, typename... _Args>
-  double calc_mean_abs_error(
-      unordered_map_with_default_value<_Key, double, _Args...> const& lhs,
-      unordered_map_with_default_value<_Key, double, _Args...>& rhs) const
+  template <typename _Ty>
+  static double calc_mean_abs_error(_Ty const& lhs, _Ty const& rhs)
   {
     long double res = 0;
     for (auto& kv : rhs)
     {
       auto lhsres = lhs.at(kv.first);
-      // lhsres *= rhs.size()/lhs.size();
       auto tres   = std::abs(kv.second - lhsres);
       res += tres;
     }
 
     return static_cast<double>(res / rhs.size());
   }
+
+  // template <typename _Key, typename... _Args>
+  // static double calc_mean_squared_error(
+  //     unordered_map_with_default_value<_Key, double, _Args...> const& lhs,
+  //     unordered_map_with_default_value<_Key, double, _Args...> const& rhs)
+  // {
+  //   long double res = 0;
+  //   for (auto& kv : rhs)
+  //     res += std::pow(kv.second - lhs.at(kv.first), 2);
+  //   return static_cast<double>(res / rhs.size());
+  // }
+  // template <typename _Key, typename... _Args>
+  // static double calc_mean_abs_error(
+  //     unordered_map_with_default_value<_Key, double, _Args...> const& lhs,
+  //     unordered_map_with_default_value<_Key, double, _Args...> const& rhs)
+  // {
+  //   long double res = 0;
+  //   for (auto& kv : rhs)
+  //   {
+  //     auto lhsres = lhs.at(kv.first);
+  //     auto tres   = std::abs(kv.second - lhsres);
+  //     res += tres;
+  //   }
+  //   return static_cast<double>(res / rhs.size());
+  // }
 
   std::string try_analyze(std::string_view msg, double alpha, double beta,
                           double precision, bool shuffle = true,
@@ -342,17 +368,12 @@ public:
     cipher.updateKey({initial, guessed, abc});
 
     std::string guess_msg = cipher.decode(msg);
-    // auto resolved_let     = count_frequencies(guess_msg);
-    // auto resolved_big     = count_bigramms(guess_msg);
-    // save_bigramms("../bigrs.csv", resolved_big);
-    auto resolved_3gr   = count_ngramms(guess_msg, 3);
-    auto resolved_1gr   = count_ngramms(guess_msg, 1);
-    auto resolved_2gr   = count_ngramms(guess_msg, 2);
-    double mse_letters  = 0;
-    double mse_bigramms = 0;
-    double mse_3gramms  = 0;
-    double mse_1gramms  = 0;
-    double mse_2gramms  = 0;
+    auto resolved_3gr     = count_ngramms(guess_msg, 3);
+    auto resolved_1gr     = count_ngramms(guess_msg, 1);
+    auto resolved_2gr     = count_ngramms(guess_msg, 2);
+    double mse_3gramms    = 0;
+    double mse_1gramms    = 0;
+    double mse_2gramms    = 0;
     double total_mse;
 #define RECALCULATE()                                                          \
   guess_msg    = cipher.decode(msg);                                           \
@@ -364,18 +385,8 @@ public:
   mse_3gramms  = calc_mean_abs_error(m_ngm2freq[2], resolved_3gr);             \
   total_mse    = alpha * mse_1gramms + beta * mse_2gramms +                    \
               (1 - alpha - beta) * mse_3gramms;
-    // mse_letters  = calc_mean_abs_error(m_symb2freq, resolved_let);               \
-  // mse_bigramms = calc_mean_abs_error(m_bigr2freq, resolved_big);               \
-  // resolved_let = count_frequencies(guess_msg);                                 \
-  // resolved_big = count_bigramms(guess_msg);                                    \
 
-    // 0.4 0.6 0.5
-    // 0.266 0.4 0.33333
-    // alpha = 0.266
-    // beta = 0.4
     RECALCULATE();
-    // mse_1gramms = calc_mean_abs_error(m_ngm2freq[0], resolved_1gr);
-
     while (total_mse > precision)
     {
       auto prev_mse = total_mse;
@@ -396,7 +407,6 @@ public:
         std::swap(guessed[a], guessed[b]);
         cipher.swap_lets(abc.enumerate(guessed[a]), abc.enumerate(guessed[b]));
         total_mse = prev_mse;
-        // RECALCULATE();
       }
 #ifdef DEBUG
       if (prev_mse != total_mse)
@@ -409,6 +419,64 @@ public:
 #endif
     }
     return guess_msg;
+  }
+
+  std::string try_analyze_affine(std::string_view msg, double alpha,
+                                 double beta, Alphabet abc) const
+  {
+    AffineCipher cipher;
+    cipher.updateAbc(abc);
+    int64_t alpha_k = 1, beta_k = 0;
+    cipher.updateKey(AffineKey{alpha_k, beta_k, abc.size()});
+
+    std::string guess_msg = cipher.decode(msg);
+    auto resolved_3gr     = count_ngramms(guess_msg, 3);
+    auto resolved_1gr     = count_ngramms(guess_msg, 1);
+    auto resolved_2gr     = count_ngramms(guess_msg, 2);
+    double mse_3gramms    = 0;
+    double mse_1gramms    = 0;
+    double mse_2gramms    = 0;
+    double total_mse;
+
+    RECALCULATE();
+    #if DEBUG
+      std::cout << "\n\n\n";
+    #endif
+    for (; alpha_k < abc.size(); alpha_k++)
+    {
+      if (std::__gcd(static_cast<size_t>(alpha_k), abc.size()) != 1)
+        continue;
+      for (beta_k = 0; beta_k < abc.size(); beta_k++)
+      {
+        auto prev_mse = total_mse;
+        auto prev_key = cipher.getKey();
+        cipher.updateKey({alpha_k, beta_k, abc.size()});
+
+        RECALCULATE();
+
+        if (total_mse > prev_mse)
+        {
+          cipher.updateKey(prev_key);
+          total_mse = prev_mse;
+        }
+#ifdef DEBUG
+          std::cout << "\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r"
+                       "\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r"
+                       "\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r"
+                    << " Total_mse: " << std::setprecision(9) << total_mse
+                    << " Mse_letters: " << std::setprecision(9)
+                    << mse_1gramms
+                    << " Mse_bigramms: " << std::setprecision(9)
+                    << mse_2gramms
+                    << " Mse_3gramms: " << std::setprecision(9)
+                    << mse_3gramms 
+                    << " Alpha:" << alpha_k
+                    << " Beta:" << beta_k << "  "
+                    << std::flush;
+#endif
+      }
+    }
+    return cipher.decode(msg);
   }
 };
 
--- dial.cpp
+++ dial.cpp
@@ -32,64 +32,19 @@ int main(int argc, char const* argv[])
   std::cout << "Affine recursive cipher:\n Result:\n  " << encr
             << "\n\n Source:\n  " << arc.decode(encr) << std::endl;
 
-  Analyzer an{"../freqs.csv", "../bigramms.csv", "../ngramms.csv"};
+  Analyzer an{"../ngramms.csv"};
 
-  // std::ofstream file1("../dumbfile.txt");
-  // Analyzer::dump_double(file1, 12.5);
-  // file1.close();
-  // std::ifstream file2("../dumbfile.txt");
-  // std::cout << Analyzer::restore_double(file2)<<std::endl;
-  // file2.close();
   std::string msg =
-#include "inittext.txt"
+// #include "inittext.txt"
+#include "testtext.txt"
       ;
-  // msg
-//    =
-//       R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//       Pellentesque fringilla, nisi maximus tempus euismod, sem mi pretium
-//       libero, non porttitor ipsum elit vel ante. Mauris elit enim, vehicula
-//       condimentum lectus quis, rutrum dapibus mauris. Nunc sed pharetra enim.
-//       Pellentesque rutrum lacus dui, non commodo velit tristique nec. Aliquam
-//       mollis, ligula nec rutrum mattis, magna lacus consequat tellus, in
-//       sodales ante lectus in sapien. Nunc accumsan massa non orci molestie
-//       rhoncus. Nulla efficitur tortor sit amet libero interdum, et tincidunt
-//       nibh dignissim. Cras et molestie lacus. Quisque vulputate turpis id
-//       eros ultricies ornare. Nunc elementum tristique erat, nec porttitor
-//       turpis dignissim vitae.
-// Nam quis nisi vel elit iaculis imperdiet. Phasellus tempus, nunc quis porta
-// egestas, odio odio malesuada nisl, vitae interdum sapien neque sed ligula.
-// Sed non libero nec felis egestas aliquet at ornare metus. Suspendisse sodales
-// nisi sed arcu elementum tristique. Curabitur ante dui, mollis ac massa vitae,
-// ornare aliquam eros. In pretium egestas nunc, id elementum risus consectetur
-// in. Etiam vehicula, metus iaculis feugiat dictum, risus augue scelerisque ex,
-// in tincidunt eros elit et elit. Suspendisse et ultricies leo. Sed et sem
-// placerat, dictum justo vitae, sagittis libero. Sed condimentum erat eu erat
-// finibus volutpat interdum ut odio. Praesent neque massa, vestibulum et
-// eleifend in, volutpat non augue. In hac habitasse platea dictumst. Aenean
-// vestibulum sit amet urna in interdum. Nullam faucibus diam sed elit ornare
-// tincidunt. Phasellus ut pretium felis, vitae dignissim leo. Nullam risus
-// ipsum, laoreet eget nulla pharetra, iaculis consectetur massa. Aliquam sapien
-// odio, semper in suscipit nec, auctor at tellus. Nullam in orci ex. Aenean
-// hendrerit mauris eu mi cursus, id rhoncus arcu euismod. Sed ultrices ligula
-// et elit faucibus, id vestibulum nisl varius. Ut malesuada sit amet ipsum eu
-// consectetur. Suspendisse a ullamcorper nisi. Donec ac laoreet orci. Donec
-// fermentum dignissim massa quis suscipit. Vivamus suscipit porttitor nisi sit
-// amet consectetur. Pellentesque non blandit ex, ut facilisis tellus. Nullam
-// scelerisque eros nunc, ut viverra elit volutpat eu. Nunc faucibus nulla non
-// nisl hendrerit, sit amet dapibus orci vestibulum. Aliquam erat volutpat.
-// Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-// inceptos himenaeos. Ut ut leo vel magna tempus laoreet. Nullam mi tortor,
-// suscipit non laoreet a, bibendum ac enim. Quisque aliquet faucibus purus
-// blandit sodales. Nunc lobortis est at elit malesuada, vel convallis lorem
-// vulputate. Nulla fringilla sollicitudin nisi, vel tempor dui convallis id.
-// Cras eleifend ullamcorper mauris, ac tempus nisl vestibulum non. Nullam
-// tristique luctus dignissim. Nullam accumsan metus in congue convallis.
-// Vivamus vehicula eget turpis vel finibus. Maecenas et massa felis.
-// Suspendisse tempor arcu turpis, ut congue eros semper sed. Suspendisse
-// gravida et arcu ac commodo. Nunc sed diam a dolor pretium consequat. Sed
-// scelerisque nibh at feugiat mattis. Fusce eget ligula dui.)";
-// // auto res1 = an.count_ngramms(msg, 4);
-// // an.save_ngramms("../ngramms.csv", res1);
+  abc.update_from_text(msg);
+  afc.updateAbc(abc);
+  afc.updateKey({static_cast<int64_t>(abc.size())-1, 5, static_cast<int64_t>(abc.size())});
+
+  auto encrypted1 = afc.encode(msg);
+  auto result1 = an.try_analyze_affine(encrypted1, 0.2, 0.3, abc);
+  std::cout << result1;
 #if 0
   auto res12 = an.count_ngramms(msg, 1);
   an.save_ngramms("../ngramms.csv", res12);
@@ -98,12 +53,7 @@ int main(int argc, char const* argv[])
   auto res14 = an.count_ngramms(msg, 3);
   an.save_ngramms("../ngramms.csv", res14);
 #endif
-  // auto freqs1 = an.count_frequencies(msg);
-  // an.save_freqs("../freqs.csv", freqs1);
-  // auto freqs = an.count_bigramms(msg);
-  // an.save_bigramms("../bigramms.csv", freqs);
 
-  abc.update_from_text(msg);
 #if 0
   std::string to_decrypt = R"(miRpD ANYtD XY YXDNsa ftDDa rpMr iL rlp NRXJrXJg UJf raNpYprrXJg XJftYrRa. miRpD ANYtD lUY bppJ rlp XJftYrRa'Y YrUJfURf ftDDa rpMr pkpR YXJyp rlp 1500Y, FlpJ UJ tJzJiFJ NRXJrpR riiz U gUsspa iL raNp UJf YyRUDbspf Xr ri DUzp U raNp YNpyXDpJ biiz. Ar lUY YtRkXkpf Jir iJsa LXkp ypJrtRXpY, btr UsYi rlp spUN XJri pspyrRiJXy raNpYprrXJg, RpDUXJXJg pYYpJrXUssa tJylUJgpf. Ar FUY NiNtsURXYpf XJ rlp 1960Y FXrl rlp RpspUYp iL mprRUYpr YlpprY yiJrUXJXJg miRpD ANYtD NUYYUgpY, UJf DiRp RpypJrsa FXrl fpYzriN NtbsXYlXJg YiLrFURp sXzp qsftY cUgpEUzpR XJystfXJg kpRYXiJY iL miRpD ANYtD.)";
   auto decr = an.try_analyze(to_decrypt, 0.2, 0.2, 0.000001, false, true);
@@ -111,6 +61,7 @@ int main(int argc, char const* argv[])
   std::string to_decrypt =
 #include "testtext.txt"
       ;
+  std::cin.get();
   auto decr = an.try_analyze(to_decrypt, 0.26666, 0.4, 0.00068, true, false);
 #endif
   std::cout << "\n\n" << decr << "\n\n";
